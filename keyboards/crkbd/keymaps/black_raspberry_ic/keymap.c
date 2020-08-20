1678a0c4cbe8022a21e170c8519e753e7f216dc7
Calvin Morooney <calvinmorooney@Calvins-MBP.fios-router.home>
2020-08-20 09:36:46 -0400

    fucking C....move tap dance declarations to bottom of file for black raspberry ic

--- keyboards/crkbd/keymaps/black_raspberry_ic/keymap.c
+++ keyboards/crkbd/keymaps/black_raspberry_ic/keymap.c
@@ -21,12 +21,6 @@ enum tap_dance_codes {
   TD_QUOT
 };
 
-// All tap dance functions go here.
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_COLON] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_colon_finished, dance_colon_reset),
-    [TD_QUOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_quote_finished, dance_quote_reset)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -127,3 +121,10 @@ void dance_colon_reset(qk_tap_dance_state_t *state, void *user_data) {
         unregister_code16(KC_COLN);
     }
 }
+
+// All tap dance functions go here.
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_COLON] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_colon_finished, dance_colon_reset),
+    [TD_QUOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_quote_finished, dance_quote_reset)
+};
+
