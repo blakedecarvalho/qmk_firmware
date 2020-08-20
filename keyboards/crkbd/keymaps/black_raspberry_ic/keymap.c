65fa82fef1ceae9be55f5a6b2fd9c4168e2fd787
Calvin Morooney <calvinmorooney@Calvins-MBP.fios-router.home>
2020-08-20 09:36:21 -0400

    puts 'tick' back on keymap (whoops) and adds a key for shift+enter for new-lines in chat software

--- keyboards/crkbd/keymaps/black_raspberry_ic/keymap.c
+++ keyboards/crkbd/keymaps/black_raspberry_ic/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_AMPR, KC_QUES,                     KC_BSLS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PIPE, KC_PSLS,                     KC_HASH, KC_AT,   KC_EXLM,  KC_TRNS, KC_TRNS, KC_TRNS,\
+      KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PIPE, KC_PSLS,                     KC_HASH, KC_AT,   KC_EXLM,  KC_TRNS, KC_TRNS, S(KC_ENT),\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_CIRC, KC_DLR,                      KC_PERC, KC_ASTR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                      KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT, KC_TRNS,  KC_TRNS,\
+      KC_GRV, KC_TRNS, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                      KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT, KC_TRNS,  KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS, KC_TRNS, KC_LT,  KC_GT,   KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
